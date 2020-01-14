9ba01c1f079205d9453ed6e0b1a5b3d76fb1b49c
reducks3000 <reducks3000@gmail.com>
2020-01-14 05:00:58 -0600

    added answers to the friends.js score array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,18 @@
 const friends = [
-    {
+  {
       name: "Adam West",
       photo: "https://pmcvariety.files.wordpress.com/2017/06/rexfeatures_5884729u.jpg?w=1000&h=563&crop=1",
       scores: [
+        "5",
+        "3",
+        "5",
+        "3",
+        "3",
+        "2",
+        "5",
+        "4",
+        "5",
+        "5"
         
       ]
     },
@@ -10,6 +20,16 @@ const friends = [
       name: "Joaquin Phoenix",
       photo: "https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/IULLSA22JJEOZODMJTDAB3QVOU&w=767",
       scores: [
+        "5",
+        "1",
+        "5",
+        "1",
+        "1",
+        "5",
+        "5",
+        "1",
+        "3",
+        "1"
         
       ]
     },
@@ -17,6 +37,16 @@ const friends = [
       name: "Kirby",
       photo: "https://aff5fa4925746bf9c161-fb36f18ca122a30f6899af8eef8fa39b.ssl.cf5.rackcdn.com/images/Masthead_Kirby.17345b1513ac044897cfc243542899dce541e8dc.9afde10b.png",
       scores: [
+        "5",
+        "1",
+        "5",
+        "1",
+        "5",
+        "1",
+        "3",
+        "1",
+        "3",
+        "5"
         
       ]
     },
@@ -24,6 +54,16 @@ const friends = [
       name: "Super Mario",
       photo: "https://www.mariowiki.com/images/thumb/2/2b/Marioptds.png/146px-Marioptds.png",
       scores: [
+        "5",
+        "3",
+        "5",
+        "2",
+        "3",
+        "3",
+        "3",
+        "2",
+        "3",
+        "5"
         
       ]
     },
@@ -31,6 +71,16 @@ const friends = [
       name: "John Wayne",
       photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/John_wayne_challenge_of_ideas_screenshot_2.jpg/220px-John_wayne_challenge_of_ideas_screenshot_2.jpg",
       scores: [
+        "2",
+        "5",
+        "5",
+        "4",
+        "2",
+        "5",
+        "5",
+        "5",
+        "1",
+        "1"
        
       ]
     },
@@ -38,6 +88,16 @@ const friends = [
       name: "Ray Romano",
       photo: "https://cdnph.upi.com/svc/sv/upi/4131526666028/2018/1/df85d8990098ad202ed3a3c763e519ef/Ray-Romano-says-Martin-Scorsese-never-heard-of-him-before-Vinyl-audition.jpg",
       scores: [
+        "4",
+        "5",
+        "2",
+        "3",
+        "1",
+        "5",
+        "3",
+        "3",
+        "2",
+        "2"
         
       ]
     }
